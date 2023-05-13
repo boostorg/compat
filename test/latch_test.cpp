cf06d842e220fa34cad7bc7f7856b6837a03ec94
Peter Dimov <pdimov@gmail.com>
2023-05-14 01:09:13 +0300

    Add stub latch_test.cpp

---
+++ test/latch_test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2023 Peter Dimov.
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/compat/latch.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+int main()
+{
+    return boost::report_errors();
+}
