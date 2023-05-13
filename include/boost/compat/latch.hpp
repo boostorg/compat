969d170d659b0771462e2c9383b648de35ea5b61
Peter Dimov <pdimov@gmail.com>
2023-05-13 21:46:06 +0300

    Add stub boost/compat/latch.hpp header

---
+++ include/boost/compat/latch.hpp
@@ -0,0 +1,17 @@
+#ifndef BOOST_COMPAT_LATCH_HPP_INCLUDED
+#define BOOST_COMPAT_LATCH_HPP_INCLUDED
+
+#include <mutex>
+#include <condition_variable>
+
+namespace boost
+{
+namespace compat
+{
+
+class latch;
+
+} // namespace compat
+} // namespace boost
+
+#endif // #ifndef BOOST_COMPAT_LATCH_HPP_INCLUDED
--- test/quick.cpp
+++ test/quick.cpp
@@ -2,6 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/compat/latch.hpp>
+
 int main()
 {
 }
