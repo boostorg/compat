aba1bd2fbe1d5a5ab54b3f172a04ce1baa132521
Peter Dimov <pdimov@gmail.com>
2024-03-27 21:54:07 +0200

    Update bind_front_fn_test

--- test/bind_front_fn_test.cpp
+++ test/bind_front_fn_test.cpp
@@ -8,6 +8,7 @@
 
 #include <boost/compat/bind_front.hpp>
 #include <boost/core/lightweight_test.hpp>
+#include <memory>
 
 int f0()
 {
@@ -29,6 +30,11 @@ int f3( int x1, int x2, int x3 ) noexcept
     return 100*x1 + 10*x2 + x3;
 }
 
+int g( std::unique_ptr<int> p, std::unique_ptr<int> q )
+{
+    return 10 * *p + *q;
+}
+
 int main()
 {
     BOOST_TEST_EQ( boost::compat::bind_front( f0 )(), -1 );
@@ -44,6 +50,8 @@ int main()
     BOOST_TEST_EQ( boost::compat::bind_front( f3, 1, 2 )( 3 ), 123 );
 
     BOOST_TEST_EQ( boost::compat::bind_front( f3, 1, 2, 3 )(), 123 );
+
+    BOOST_TEST_EQ( boost::compat::bind_front( g, std::unique_ptr<int>( new int(1) ) )( std::unique_ptr<int>( new int(2) ) ), 12 );
 
     return boost::report_errors();
 }
