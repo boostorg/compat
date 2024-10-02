b59ba15ceb22a123e52b1669b31849f35fb93ce6
Ruben Perez <rubenperez038@gmail.com>
2024-10-02 16:31:56 +0200

    Disable rvalue tests for MSVC 14.0

---
+++ test/to_array_lvalue_test.cpp
@@ -0,0 +1,42 @@
+// Copyright 2024 Ruben Perez Hidalgo.
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/compat/to_array.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+#include <array>
+#include <vector>
+
+namespace compat = boost::compat;
+
+int main()
+{
+    {
+        // regular C array
+        int input[] = {1, 2};
+        auto output = compat::to_array(input);
+        static_assert(std::is_same<decltype(output), std::array<int, 2>>::value, "");
+        BOOST_TEST_EQ(output[0], 1);
+        BOOST_TEST_EQ(output[1], 2);
+    }
+    {
+        // values not moved
+        const std::vector<int> vec{1, 2};
+        std::vector<int> input[]{vec};
+        auto output = compat::to_array(input);
+        static_assert(std::is_same<decltype(output), std::array<std::vector<int>, 1>>::value, "");
+        BOOST_TEST_ALL_EQ(output[0].begin(), output[0].end(), vec.begin(), vec.end());
+        BOOST_TEST_ALL_EQ(input[0].begin(), input[0].end(), vec.begin(), vec.end());  // input not modified
+    }
+    {
+        // constexpr check
+        // Most array members are not constexpr until late C++ standards,
+        // so we just test that code compiles
+        constexpr int input[] = {1, 2, 3};
+        constexpr auto output = compat::to_array(input);
+        static_cast<void>(output);
+    }
+
+    return boost::report_errors();
+}
