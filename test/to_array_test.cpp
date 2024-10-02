d6446bbf606092cd491d4f45a4f7ec09b54b89f7
Ruben Perez <rubenperez038@gmail.com>
2024-10-02 12:09:28 +0200

    Workaround MSVC problems with moving arrays

--- test/to_array_test.cpp
+++ test/to_array_test.cpp
@@ -33,7 +33,7 @@ int main()
     {
         // rvalue, regular C array
         int input[] = {5, 6};
-        auto output = compat::to_array(std::move(input));
+        auto output = compat::to_array(static_cast<decltype(input)&&>(input));
         static_assert(std::is_same<decltype(output), std::array<int, 2>>::value, "");
         BOOST_TEST_EQ(output[0], 5);
         BOOST_TEST_EQ(output[1], 6);
@@ -42,7 +42,7 @@ int main()
         // rvalue, values moved
         const std::vector<int> vec{1, 2};
         std::vector<int> input[]{vec};
-        auto output = compat::to_array(std::move(input));
+        auto output = compat::to_array(static_cast<decltype(input)&&>(input));
         static_assert(std::is_same<decltype(output), std::array<std::vector<int>, 1>>::value, "");
         BOOST_TEST_ALL_EQ(output[0].begin(), output[0].end(), vec.begin(), vec.end());
         BOOST_TEST(input[0].empty());  // input left in a moved-from state
@@ -51,7 +51,7 @@ int main()
         // rvalue, move-only types work
         std::unique_ptr<int> input[] = {std::unique_ptr<int>{new int(42)}};
         int* ptr = input[0].get();
-        auto output = compat::to_array(std::move(input));
+        auto output = compat::to_array(static_cast<decltype(input)&&>(input));
         static_assert(std::is_same<decltype(output), std::array<std::unique_ptr<int>, 1>>::value, "");
         BOOST_TEST_EQ(output[0].get(), ptr);
         BOOST_TEST_EQ(input[0].get(), nullptr);  // input left in a moved-from state
