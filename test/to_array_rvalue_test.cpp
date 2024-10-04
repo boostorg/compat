c18e5f2276367f53a6ba496744983d517197ee57
Peter Dimov <pdimov@gmail.com>
2024-10-04 13:26:50 +0300

    Move MSVC workarounds from test/Jamfile to to_array_rvalue_test.cpp

--- test/to_array_rvalue_test.cpp
+++ test/to_array_rvalue_test.cpp
@@ -4,11 +4,21 @@
 
 #include <boost/compat/to_array.hpp>
 #include <boost/core/lightweight_test.hpp>
+#include <boost/config.hpp>
+#include <boost/config/workaround.hpp>
+#include <boost/config/pragma_message.hpp>
 
 #include <array>
 #include <memory>
 #include <utility>
 #include <vector>
+
+#if defined(BOOST_MSVC) && BOOST_MSVC < 1910
+
+BOOST_PRAGMA_MESSAGE( "Test skipped because BOOST_MSVC < 1910" )
+int main() {}
+
+#else
 
 namespace compat = boost::compat;
 
@@ -37,7 +47,9 @@ int main()
         auto output = compat::to_array(std::move(input));
         static_assert(std::is_same<decltype(output), std::array<std::vector<int>, 1>>::value, "");
         BOOST_TEST_ALL_EQ(output[0].begin(), output[0].end(), vec.begin(), vec.end());
+#if !BOOST_WORKAROUND(BOOST_MSVC, < 1920)
         BOOST_TEST(input[0].empty());  // input left in a moved-from state
+#endif
     }
     {
         // const values work (although they don't result in an effective move)
@@ -48,6 +60,7 @@ int main()
         BOOST_TEST_ALL_EQ(output[0].begin(), output[0].end(), vec.begin(), vec.end());
         BOOST_TEST_ALL_EQ(input[0].begin(), input[0].end(), vec.begin(), vec.end());  // input not modified
     }
+#if !BOOST_WORKAROUND(BOOST_MSVC, < 1920)
     {
         // move-only types work
         std::unique_ptr<int> input[] = {std::unique_ptr<int>{new int(42)}};
@@ -57,6 +70,8 @@ int main()
         BOOST_TEST_EQ(output[0].get(), ptr);
         BOOST_TEST_EQ(input[0].get(), nullptr);  // input left in a moved-from state
     }
+#endif
+
     {
         // constexpr check
         constexpr int input[] = {1, 2, 3};
@@ -69,3 +84,5 @@ int main()
 
     return boost::report_errors();
 }
+
+#endif
