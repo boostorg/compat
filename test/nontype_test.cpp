904b80b8a1c142903928fb504d99c8cc29047b80
Peter Dimov <pdimov@gmail.com>
2025-11-18 11:23:41 +0200

    Move nontype.hpp to detail

--- test/nontype_test.cpp
+++ test/nontype_test.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/compat/nontype.hpp>
+#include <boost/compat/detail/nontype.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <boost/core/lightweight_test_trait.hpp>
 #include <boost/config/pragma_message.hpp>
